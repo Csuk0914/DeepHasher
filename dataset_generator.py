75f4dd6f79958fb45edfb60468a35a7f79b5c370
Baichuan Jiang <baichuan@jhu.edu>
2019-01-01 18:03:39 -0500

    Generate test images.

--- dataset_generator.py
+++ dataset_generator.py
@@ -18,6 +18,7 @@ label_test = np.zeros([num_test_imgs, 9])
 # data_test_path = "../data/bjiang8/data_test/"
 # for camp pc
 data_train_path = "X:/Baichuan_Files/data/data_train/"
+data_test_path = "X:/Baichuan_Files/data/data_test/"
 
 # Read volume data from /test folder
 dirname = '../test'
@@ -77,3 +78,30 @@ for img_id in range(num_train_imgs):
 
 label_pth = os.path.join(data_train_path, 'label.csv')
 np.savetxt(label_pth, label_train, delimiter=",")
+
+# Generate testing dataset
+for img_id in range(num_test_imgs):
+    # Transform images
+    f = randTrans4x4(debug=False)
+    trans_pts = np.matmul(f, source_pts)
+    for i in range(slice_sz):
+        for j in range(slice_sz):
+            trans_pts[0, i*slice_sz + j] = trans_pts[0, i*slice_sz + j] + slice_sz/2
+            trans_pts[1, i*slice_sz + j] = trans_pts[1, i*slice_sz + j] + slice_sz/2
+    trans_pts = np.transpose(trans_pts)
+    interp_vals = volume_interp_func(trans_pts[:,0:3])
+    random_slice  = np.reshape(interp_vals.astype('uint8'),(slice_sz,slice_sz))
+    # Process label
+    trans_anchor_pts = np.matmul(f, source_anchor_pts)
+    label = trans_anchor_pts[0:3, :].flatten('F')
+    label_test[img_id, :] = label
+    # Save image
+    im = Image.fromarray(random_slice)
+    img_pth = os.path.join(data_test_path, 'img_(%d).png' % img_id)
+    im.save(img_pth)
+    # Print out process
+    if img_id % (num_test_imgs/100) == 0:
+        print("Generated %d images, of total %d images" % (img_id, num_test_imgs))
+
+label_pth = os.path.join(data_test_path, 'label.csv')
+np.savetxt(label_pth, label_test, delimiter=",")
