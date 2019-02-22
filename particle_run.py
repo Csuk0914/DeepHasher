b3ec37d990f13a3f0c77a0a678d379fdc8713031
Baichuan Jiang <baichuan@jhu.edu>
2019-02-21 21:15:25 -0500

    Particle filter test.

---
+++ particle_run.py
@@ -0,0 +1,158 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from pfilter import ParticleFilter, gaussian_noise, squared_error, independent_sample
+from scipy.stats import norm, gamma, uniform
+
+# ------------- Open data -------------
+#
+with open("../data_series/series5/label.csv") as csvfile:
+    data = csv.reader(csvfile, delimiter=",")
+    labels = []
+    rownum = 0
+    for row in data:
+        labels.append(row)
+        labels[rownum] = [float(i) for i in labels[rownum]]
+        rownum += 1
+
+with open("../data_series/series5/predict.csv") as csvfile:
+    data = csv.reader(csvfile, delimiter=",")
+    preds = []
+    rownum = 0
+    for row in data:
+        preds.append(row)
+        preds[rownum] = [float(i) for i in preds[rownum]]
+        rownum += 1
+
+nppreds = np.asarray(preds)
+nplabel = np.asarray(labels)
+
+# ------------- Particle filter setup -------------
+#
+columns = ["x", "y", "z", "dx", "dy", "dz"]
+prior_fn = independent_sample(
+    [
+        # Location
+        norm(loc=nppreds[0,0], scale=20).rvs,
+        norm(loc=nppreds[0,1], scale=20).rvs,
+        norm(loc=nppreds[0,2], scale=20).rvs,
+        # Speed
+        norm(loc=0, scale=0.5).rvs,
+        norm(loc=0, scale=0.5).rvs,
+        norm(loc=0, scale=0.5).rvs,
+    ]
+)
+
+def ob_fn(x):
+    # observation function
+    y = np.zeros((x.shape[0], 3))
+    for i, particle in enumerate(x):
+        y[i,0] = norm(loc=particle[0], scale=2).rvs(1)
+        y[i,1] = norm(loc=particle[1], scale=2).rvs(1)
+        y[i,2] = norm(loc=particle[2], scale=2).rvs(1)
+    return y
+
+# very simple linear dynamics: x += dx
+def velocity(x):
+    dt = 1.0
+    xp = (
+        x
+        @ np.array(
+            [
+                [1, 0, 0, dt, 0],
+                [0, 1, 0, 0, dt],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1],
+            ]
+        ).T
+    )
+
+    return xp
+
+def process_fn(x):
+    # Dynamic process function
+    dt = 1.0
+    xp = (
+        x
+        @ np.array([[1.,0,0,dt, 0, 0],
+                 [0,1.,0,0, dt, 0],
+                 [0,0,1.,0, 0, dt],
+                 [0,0,0,1., 0, 0],
+                 [0,0,0,0, 1., 0],
+                 [0,0,0,0, 0, 1.]]).T
+    )
+    return xp
+
+def rbf_error(x, y, sigma=1):
+    # RBF kernel
+    d = np.sum((x - y) ** 2, axis=1)
+    return np.exp(-d / (2.0 * sigma ** 2))
+
+pf = ParticleFilter(
+    prior_fn=prior_fn,
+    observe_fn=ob_fn,
+    n_particles=100,
+    dynamics_fn=process_fn,
+    noise_fn=lambda x: gaussian_noise(x, sigmas=[5, 5, 5, 0.05, 0.05,0.1]),
+    weight_fn=lambda x, y: rbf_error(x, y, sigma=2),
+    resample_proportion=0.2,
+    column_names=columns,
+)
+
+# ------------- Running Particle filter -------------
+#
+pf.init_filter()
+pred_filt_rec = np.zeros([nppreds.shape[0]-1, 3])
+for i in range(nppreds.shape[0]-1):
+    pf.update(nppreds[i+1,0:3])
+    x_hat, y_hat, z_hat, dx_hat, dy_hat, dz_hat = pf.mean_state
+    pred_filt_rec[i,0] = x_hat
+    pred_filt_rec[i,1] = y_hat
+    pred_filt_rec[i,2] = z_hat
+
+
+# ------------- Numerical comparison -------------
+#
+total_diff_1 = 0
+total_diff_2 = 0
+for i in range(nplabel.shape[0]-1):
+    diff1 = np.linalg.norm(nplabel[i+1,3:6] - nppreds[i+1,0:3])
+    total_diff_1 += diff1
+    diff2 = np.linalg.norm(nplabel[i+1,3:6] - pred_filt_rec[i,0:3])
+    total_diff_2 += diff2
+print("Mean error before filt: ", total_diff_1/nplabel.shape[0])
+print("Mean error after filt: ", total_diff_2/nplabel.shape[0])
+
+# -------------  Plot the trajectories -------------
+#
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+x = nplabel[:,3]
+y = nplabel[:,4]
+z = nplabel[:,5]
+ax.plot(x, y, z, label='ground truth')
+
+x = nppreds[:,0]
+y = nppreds[:,1]
+z = nppreds[:,2]
+ax.plot(x, y, z, label='raw predicts')
+
+x = pred_filt_rec[:,0]
+y = pred_filt_rec[:,1]
+z = pred_filt_rec[:,2]
+ax.plot(x, y, z, label='filtered predicts')
+
+ax.legend()
+ax.set_xlim3d([0, 250])
+ax.set_ylim3d([-350, -100])
+ax.set_zlim3d([-250, 0])
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_aspect('equal')
+# ax.view_init(elev=20., azim=-35)
+
+plt.show()
